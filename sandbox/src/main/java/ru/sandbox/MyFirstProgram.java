ab60a8e3c5f5005c3b833a8dd20d0ff0fc2300ac
Igor Mashkov <wismu@yandex.ru>
2019-06-30 22:45:44 +0300

    some changes

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -4,17 +4,28 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    hello("world");
-    hello("igoryan");
+    //hello("world");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    //Square s = new Square(5);
+    //System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4,8);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    //Rectangle r = new Rectangle(4,8);
+    //System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+    Point p1 = new Point(6.0,6.0);
+    Point p2 = new Point (2.0,2.0);
+    //System.out.println("Координаты первой точки равны: " +"(" + p1.x +":" + p1.y + ")");
+    //System.out.println("Координаты второй точки равны: " +"(" + p2.x +":" + p2.y + ")");
+    System.out.println("Расстояние между точками равно:  " + distance(p1,p2));
+
   }
 
-  public static void hello(String somewhat) {
-    System.out.println("Hello, " + somewhat + "!");
+  //public static void hello(String somewhat) {
+   // System.out.println("Hello, " + somewhat + "!");
+  //}
+
+    public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+    }
   }
-}
---
+++ sandbox/src/main/java/ru/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.sandbox;
+
+public class Point{
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+
+  }
