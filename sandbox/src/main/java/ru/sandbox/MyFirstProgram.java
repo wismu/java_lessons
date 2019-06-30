43af969e9733578636a0794ecb72251d6a0514bb
Igor Mashkov <wismu@yandex.ru>
2019-06-30 11:58:48 +0300

    added some functions

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -4,6 +4,31 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, WORLD!");
+    hello("test");
+    hello("igoryan");
+
+    double l = 5;
+    System.out.println("Ploshad kvadrata so storonoi " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 8;
+
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+
+  public static void hello(String somewhat) {
+
+    System.out.println("Hello, " + somewhat + "!");
+
+  }
+
+
+  public static double area(double len) {
+    return len*len;
+  }
+
+  public static double area(double a, double b) {
+    return a*b;
   }
 }
