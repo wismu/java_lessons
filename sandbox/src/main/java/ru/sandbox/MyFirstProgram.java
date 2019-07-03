871d71f493d76b1161c83fc14f6732ad8a8ee232
imashkov <wismu@yandex.ru>
2019-07-03 17:09:04 +0300

    Some functions added

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -1,31 +1,45 @@
 package ru.sandbox;
 
+import javax.xml.transform.Result;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    //hello("world");
+    //Point p1 = new Point(2.0,2.0);
+    //Point p2 = new Point (6.0,6.0);
 
-    //Square s = new Square(5);
-    //System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    //System.out.println("Расстояние между точками равно:  " + distance(p1,p2));
 
-    //Rectangle r = new Rectangle(4,8);
-    //System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-
-
-    Point p1 = new Point(6.0,6.0);
-    Point p2 = new Point (2.0,2.0);
-    //System.out.println("Координаты первой точки равны: " +"(" + p1.x +":" + p1.y + ")");
-    //System.out.println("Координаты второй точки равны: " +"(" + p2.x +":" + p2.y + ")");
-    System.out.println("Расстояние между точками равно:  " + distance(p1,p2));
+    check(0,0,0,0,0);
+    check(1,0,0,0,1);
+    check(0,1,0,0,1);
+    check(0,0,1,0,1);
+    check(0,0,0,1,1);
+    check(-1,0,0,1,Math.sqrt(2));
 
   }
-
-  //public static void hello(String somewhat) {
-   // System.out.println("Hello, " + somewhat + "!");
-  //}
 
     public static double distance(Point p1, Point p2) {
     return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
     }
+
+    public static void check(double x1, double y1,double x2, double y2, double expected_distance) {
+
+    Point p1 = new Point(x1,y1);
+    Point p2 = new Point(x2,y2);
+
+    double dist = distance(p1,p2);
+
+    if (dist == expected_distance) {
+      System.out.println("Тест пройден!");
+            }
+    else {
+      System.out.println("Тест упал!");
+    }
+
+
+
+    }
+
   }
--- sandbox/src/main/java/ru/sandbox/Point.java
+++ sandbox/src/main/java/ru/sandbox/Point.java
@@ -1,15 +1,17 @@
 package ru.sandbox;
 
+import javax.xml.transform.Result;
+
 public class Point{
 
   public double x;
   public double y;
 
+
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
-  }
 
+     }
 
-
-  }
+    }
