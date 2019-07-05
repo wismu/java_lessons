afde913b140352477d0e4c3be42e866a07af81bc
Igor Mashkov <wismu@yandex.ru>
2019-07-05 18:38:46 +0300

    Исправлено замечание

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -1,12 +1,8 @@
 package ru.sandbox;
 
-import javax.xml.transform.Result;
-
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-
-
 
     check(0,0,0,0,0);
     check(1,0,0,0,1);
@@ -17,35 +13,24 @@ public class MyFirstProgram {
     Point p1 = new Point(2.0,2.0);
     Point p2 = new Point (6.0,6.0);
 
-    System.out.println("Расстояние между точками равно:  " + Point.distance(p1,p2));
+    System.out.println("Расстояние между точками равно:  " + p1.distance(p2));
 
   }
-
-    //public static double distance(Point p1, Point p2) {
-    //return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
-    //}
 
     public static void check(double x1, double y1,double x2, double y2, double expected_distance) {
 
     Point p1 = new Point(x1,y1);
     Point p2 = new Point(x2,y2);
 
-    double dist = Point.distance(p1,p2); //p1.distance(p2);
-
+    double dist = p1.distance(p2);
     if (dist == expected_distance) {
       System.out.println("Тест пройден!");
             }
     else {
       System.out.println("Тест упал!");
     }
-    //System.out.println(dist);
-
-
 
     }
-
-
-
 
 
   }
--- sandbox/src/main/java/ru/sandbox/Point.java
+++ sandbox/src/main/java/ru/sandbox/Point.java
@@ -1,7 +1,5 @@
 package ru.sandbox;
 
-import javax.xml.transform.Result;
-
 public class Point{
 
   public double x;
@@ -13,9 +11,9 @@ public class Point{
     this.y = y;
 
      }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+  public double distance(Point p2) {
+    return Math.sqrt((p2.x-x)*(p2.x-x) + (p2.y-y)*(p2.y-y));
   }
 
     }
+
