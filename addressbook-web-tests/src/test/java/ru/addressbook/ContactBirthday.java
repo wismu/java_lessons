ea4aeff46cb528a3395abc52e21bc0e5bc1f6061
Igor Mashkov <wismu@yandex.ru>
2019-07-14 12:09:19 +0300

    Добавлен ContactCreationTest

---
+++ addressbook-web-tests/src/test/java/ru/addressbook/ContactBirthday.java
@@ -0,0 +1,25 @@
+package ru.addressbook;
+
+public class ContactBirthday {
+  private final String day;
+  private final String month;
+  private final String year;
+
+  public ContactBirthday(String day, String month, String year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public String getDay() {
+    return day;
+  }
+
+  public String getMonth() {
+    return month;
+  }
+
+  public String getYear() {
+    return year;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package ru.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String address;
+  private final String phone;
+  private final String email;
+
+  public ContactData(String firstname, String middlename, String lastname, String nickname, String address, String phone, String email) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
