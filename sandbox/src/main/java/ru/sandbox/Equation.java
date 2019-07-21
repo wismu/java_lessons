582fe546c93ae0fd8fb14e46fa5a1587c114925e
Igor Mashkov <wismu@yandex.ru>
2019-07-21 17:05:33 +0300

    Добавлены тесты и улучшены класс Equation и тест EquationTests

--- sandbox/src/main/java/ru/sandbox/Equation.java
+++ sandbox/src/main/java/ru/sandbox/Equation.java
@@ -14,22 +14,29 @@ public class Equation {
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if(d>0) {
-      n = 2;
-    }
-    else {
-      if(d==0) {
+    if (a != 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
-      }
-      else {
+      } else {
         n = 0;
       }
+    } else
+      if (b != 0) {
+        n = 1; }
+      else if (c != 0) {
+      n = 0;
+    } else {
+      n = -1;
+      }
+
+  }
+
+    public int rootNubmer () {
+      return (int) n;
     }
   }
 
-  public int rootNubmer(){
-    return (int) n;
-  }
-}
--- sandbox/src/test/java/ru/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/sandbox/EquationTests.java
@@ -23,4 +23,23 @@ public class EquationTests {
     Assert.assertEquals(e.rootNubmer(), 2);
   }
 
+  @Test
+  public void testLinear() {
+    Equation e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNubmer(), 1);
+  }
+
+  @Test
+  public void testConstant() {
+    Equation e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNubmer(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNubmer(), -1);
+  }
+
+
 }
