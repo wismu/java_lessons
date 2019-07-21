1cf7aacd30ea49db892e75719d6c74e95909978c
Igor Mashkov <wismu@yandex.ru>
2019-07-21 16:26:29 +0300

    Добавлен класс Equation и тест EquationTests

---
+++ sandbox/src/main/java/ru/sandbox/Equation.java
@@ -0,0 +1,35 @@
+package ru.sandbox;
+
+public class Equation {
+
+  private double a;
+  private double b;
+  private double c;
+
+  private double n;
+
+  public Equation(double a, double b, double c) {
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b*b - 4*a*c;
+
+    if(d>0) {
+      n = 2;
+    }
+    else {
+      if(d==0) {
+        n = 1;
+      }
+      else {
+        n = 0;
+      }
+    }
+  }
+
+  public int rootNubmer(){
+    return (int) n;
+  }
+}
