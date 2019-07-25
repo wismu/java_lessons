734122e7abba144c322f808d4db46c5c15027cf2
imashkov <wismu@yandex.ru>
2019-07-25 16:22:21 +0300

    Реализована проверка и обеспечение выполнения предусловия.

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,11 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("Igor group", "stroka 1", "stroka 2"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("Igor group", "stroka 1", "stroka 2"));
+//    app.getGroupHelper().initGroupCreation();
+//    app.getGroupHelper().fillGroupForm(new GroupData("Igor group", "stroka 1", "stroka 2"));
+//    app.getGroupHelper().submitGroupCreation();
+//    app.getGroupHelper().returnToGroupPage();
 
   }
 
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Igor group", "stroka 1", "stroka 2"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Igor group", "String nomer 1", "String nomer 2"));
